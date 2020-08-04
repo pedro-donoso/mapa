9b899fb9a7655e072695ba4c19130a488fd88ef0
pedro-donoso <pedro.donoso.r@usach.cl>
2020-08-04 01:24:29 -0400

    agrego coordenadas
    latitud y longitud

---
+++ main.js
@@ -0,0 +1,22 @@
+const map = L.map('map', {
+    center: [40.954, -5.667],
+    zoom: 16
+});
+const osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//Iconos de bootstrap
+const miMarker = L.AwesomeMarkers.icon({
+    icon: 'tower',
+    prefix: 'glyphicon',
+    markerColor: 'red'
+});
+const monumentos = function (feature, marker) {
+    marker.setIcon(miMarker);
+    marker.bindPopup("<ul><h3>" + feature.properties.Nombre + " </h3><li>Estilo: " + feature.properties.Estilo
+        + "</li><li>Siglo: " + feature.properties.Siglo + "</li><li>Latitud: " + feature.geometry.coordinates[1] + "</li><li>Longitud: " + feature.geometry.coordinates[0] + "</li></ul>");
+};
+
+//Arqueoloxía		
+const monuSalamanca = new L.geoJSON(geojson, {
+    onEachFeature: monumentos
+}).addTo(map);
